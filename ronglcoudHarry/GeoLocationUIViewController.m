6d99cfdb4c08aff5b99911d2d5d191ce438a1e46
harryfeng <harry.yuanfeng@gmail.com>
2016-07-12 21:53:03 +0800

    geolocation

--- ronglcoudHarry/GeoLocationUIViewController.m
+++ ronglcoudHarry/GeoLocationUIViewController.m
@@ -7,15 +7,66 @@
 //
 
 #import "GeoLocationUIViewController.h"
-
-@interface GeoLocationUIViewController (){
-    
+#import <CoreLocation/CoreLocation.h>
+@interface GeoLocationUIViewController ()<CLLocationManagerDelegate>{
+    CLLocationManager *locationManager;
+    UITextField *longitudeLabel;
+    UITextField *latitudeLabel;
 }
 @end
 @implementation GeoLocationUIViewController
-
+// refer to this link http://stackoverflow.com/questions/4152003/how-can-i-get-current-location-from-user-in-ios
+//and this link http://www.appcoda.com/how-to-get-current-location-iphone-user/
 -(void)viewDidLoad{
+    [self setupLocationInitService];
+    [self setupSomeUI];
     
+}
+
+-(void)setupLocationInitService{
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+        [locationManager requestWhenInUseAuthorization];
+    
+    [locationManager startUpdatingLocation];
+}
+
+-(void)setupSomeUI{
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    
+    longitudeLabel = [[UITextField alloc] initWithFrame:CGRectMake(100, 100, 100, 30)];
+    longitudeLabel.textColor = [UIColor blackColor];
+    [self.view addSubview:longitudeLabel];
+    
+    latitudeLabel = [[UITextField alloc] initWithFrame:CGRectMake(100, 140, 100, 30)];
+    latitudeLabel.textColor = [UIColor blackColor];
+    [self.view addSubview:latitudeLabel];
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"didUpdateToLocation: %@", newLocation);
+    CLLocation *currentLocation = newLocation;
+    
+    if (currentLocation != nil) {
+        longitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+        latitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
+    }
+    [locationManager stopUpdatingLocation];
 }
 
 @end
